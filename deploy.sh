fa6eedf922e44e195335c649b020fa9a751f425f
zhouxiaodong <xiaodong.zhou@vivo.com>
2019-07-08 15:43:22 +0800

    modify deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -12,6 +12,6 @@ cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:vivoquickapp/apex-ui-docs.git master
+git push -f git@github.com:vivoquickapp/apex-ui-docs.git gh-pages
 
 cd -
